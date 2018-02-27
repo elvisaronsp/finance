a5a995a1d43559076751cef2dba6cb94e39f8ab4
vinil2001 <vinil2001@ukr.net>
2018-02-27 21:09:27 +0200

    First_commit_27.02.2018

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Finance.Models; 
+
+namespace Finance.Controllers
+{
+    //[Authorize]
+    public class HomeController : Controller
+    {
+
+        ApplicationDbContext Db = new ApplicationDbContext();
+
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+   
+        public ActionResult MenuBar()
+        {
+            return PartialView();
+        }
+    }
+}
